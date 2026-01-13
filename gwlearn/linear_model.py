84776670c31a0cac56d31d7ca170e914c90884ee
theralavineela <theralavineela@gmail.com>
2026-01-13 19:40:31 +0530

    Update linear_model.py

--- gwlearn/linear_model.py
+++ gwlearn/linear_model.py
@@ -6,7 +6,7 @@ import geopandas as gpd
 import numpy as np
 import pandas as pd
 from libpysal import graph
-from sklearn.linear_model import LinearRegression, LogisticRegression as SklearnLogisticRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression 
 from sklearn.utils.metadata_routing import _MetadataRequester
 from .base import BaseClassifier, BaseRegressor
 class LogisticRegression(BaseClassifier, _MetadataRequester):
